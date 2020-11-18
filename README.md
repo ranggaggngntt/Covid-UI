0d701fce309bfe93cb8166b27dfb94b28a97b5a1
ranggaggngntt <rangga@localhost.localdomain>
2020-11-18 18:36:17 +0700

    Release app

---
+++ README.md
@@ -0,0 +1,8 @@
+# Flutter Covid UI
+
+#Scratch From
+https://dribbble.com/shots/11015463-Covid-19-App-Free
+
+### Some Screenshots
+
+<img src="Covid-Screenshot.jpg" height="300em" />
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:covid_ui/screens/bottom_nav_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Covid-19 Dashboard UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomNavScreen(),
+    );
+  }
+}
